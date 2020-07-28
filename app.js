72b37d2b14692dd2c7f53cf15b963d1ff5638e0e
Nishant <renegade.plays03@gmail.com>
2020-07-28 21:24:34 +0530

    made login page

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
-
+const ejs = require("ejs")
 const app = express();
+const bp = require('body-parser')
+app.set('view engine', 'ejs');
+app.use(bp.urlencoded({extended: true}));
+app.use(express.static("public"));
 
+
+const port = 3000 || process.env.PORT
+
+
+app.use("/admindash",require("./routes/api/admindash"))
+
+app.get("/",(req,res)=>{
+  res.render("home")
+})
+
+app.post("/",(req,res)=>{
+  console.log(req.body);
+})
 app.listen(3000,()=>{
   console.log("Server Up and running on port 3000");
 })
